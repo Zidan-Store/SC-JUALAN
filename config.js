35f0108a6951da1ff528d5a18fc094221b407cfc
Tio <94074076+BOTCAHX@users.noreply.github.com>
2022-02-14 23:10:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 gc1 = 'https://chat.whatsapp.com/JEDQfzMGZan4HA98wtxsNL'
-gc2 = 'https://chat.whatsapp.com/BTQU2U9WetrFiYpqQ634CO'
+gc2 = ''
 gc3 = ''
 global.linkGC = ['https://chat.whats', 'https://chat.whatsapp.com/JEDQfzMGZan4HA98wtxsNL'] // ganti jadi group lu
-global.owner = ['6281380311018', '6282221792667', '6282320830684'] // Put your number here //owner eval
-global.kontak = ['6281380311018', '6282221792667', '6282320830684'] //Ketika ada yang ngetik #owner
+global.owner = ['6285828764046', '6282221792667', '6282320830684'] // Put your number here //owner eval
+global.kontak = ['6285828764046', '6282221792667', '6282320830684'] //Ketika ada yang ngetik #owner
 global.mods = ['0'] // Want some help?
-global.prems = ['6282221792667', '6281380311018', '6282320830684'] // Premium user has unlimited limit
+global.prems = ['6282221792667', '6285828764046', '6282320830684'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com', 
@@ -41,7 +41,7 @@ xteamkey = 'cristian9407'
 namaig = 'GAK ADA :)'
 namagithub = 'GAK ADA :)'
 kasihcaption = `Done`
-namakontak1 = 'Owner Tegar'
+namakontak1 = 'Owner Rozi'
 namakontak2 = 'Bot Creator'
 
 //kasihcaption = `Done`
