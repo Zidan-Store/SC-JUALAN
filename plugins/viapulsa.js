32037e0f3468977345b7299fcc803fcaba344653
Tio <94074076+BOTCAHX@users.noreply.github.com>
2022-02-20 13:38:48 +0700

    Update viapulsa.js

--- plugins/viapulsa.js
+++ plugins/viapulsa.js
@@ -2,9 +2,8 @@ let handler = async (m, { conn }) => {
 	conn.reply(m.chat, `╠═〘 PEMBAYARAN 〙 ═
 ╠═ Untuk Pembayaran Via Pulsa
 ╠➥ Ke nomer ini
-║ 081380311018 (Owner)
+║ 089504575881 
 ║
-║➥ 081395861695 (Creator)
 ║
 ╠═〘 Fitur 〙 ═
 ║
