bcc85dcc578e9a557bc5a3f2a53486278ffd7e99
Zidan-Store <100951047+Zidan-Store@users.noreply.github.com>
2022-03-04 23:49:59 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,11 +1,11 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'donasi')).buffer(), `
 ┌〔 Donasi • Emoney 〕
-├ PULSA : 089504575881
-├ DANA : 089504575881 
-├ OVO : 089504575881
-├ Shoppe : 089504575881
-├ GOPAY : 089504575881 
+├ PULSA : 085810172953
+├ DANA : 085810172953 
+├ OVO : TIDAK TERSEDIA∅
+├ Shoppe : TIDAK TERSEDIA∅
+├ GOPAY : TIDAK TERSEDIA∅
 `.trim(), footer, 'Owner', '.owner')
 handler.help = ['donasi']
 handler.tags = ['info']
