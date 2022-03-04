186766573ade67ccb85e77f76cd4e3862ae8b8b2
Zidan-Store <100951047+Zidan-Store@users.noreply.github.com>
2022-03-04 23:33:13 +0700

    Update belibot.js

--- plugins/belibot.js
+++ plugins/belibot.js
@@ -1,20 +1,20 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => {
 	conn.send3ButtonLoc(m.chat, await (await fetch(fla + 'sewa bot')).buffer(), `╠═〘 Beli Bot 〙 ═
-╠➥ *1 Bulan* :      *Rp 15000*
-╠➥ *Permanen* : *Rp 25000*
-╠➥ *Premium* :   *Rp 10000*
-╠➥ *Sc Bot* :        *Beli*
+╠➥ *1 Bulan* :      *Rp 20000*
+╠➥ *Permanen* : *Rp 30000*
+╠➥ *Premium* :   *Rp 15000*
+╠➥ *Sc Bot* :        *Rp 40000*
 ║
 ╠═〘 PEMBAYARAN 〙 ═
-╠➥ Gopay, Dana, Dan Pulsa
+╠➥ Dana, Dan Pulsa
 ║
 ╠═ Tertarik Untuk Beli Bot Ini?
 ╠➥Ketuk Tombol Di Bawah Ya
 ║
 ╠═ ©2021 
-╠═ Scrip original by Tio
-╠═〘 ${namabot} 〙 ═`.trim(), footer, 'Dana', '#viadana', 'OVO', '#viaovo', 'Pulsa', '#viapulsa', m)
+╠═ Scrip original by I`M 𝚉𝚒𝚍𝚊𝚗
+╠═〘 ${KiritoBot} 〙 ═`.trim(), footer, 'Dana', '#viadana', 'Pulsa', '#viapulsa', m)
 }
 
 handler.command = /^sewa(bot)?$/i
